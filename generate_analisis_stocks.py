c17b8dd83a8c72c366aa8ea04555b5d734c18687
Cristhyan Alves <97294330+cristhyan23@users.noreply.github.com>
2024-03-10 11:45:54 -0300

    Add files via upload

---
+++ generate_analisis_stocks.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+import yfinance as yf
+import numpy as np
+from datetime import date, timedelta
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout,InputLayer
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+from stockslist import Stocklist
+from sklearn.metrics import r2_score
+import unicodedata
+import sys
+import codecs
+
+class AnalisisStock(Stocklist):
+    def __init__(self):
+        super().__init__()
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.result_dict = {}
+        self.look_back = 7
+        sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+        self.epochs_database = 30
+        self.dias_base_estudo = 5000
+
+    # Função para substituir ou remover caracteres não suportados
+    def sanitize_text(self, text):
+        sanitized_text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
+        return sanitized_text
+    
+    # Função para imprimir texto codificado corretamente
+    def print_encoded(self, text):
+        sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())  # UTF-8 output
+        print(text)  
+
+# Função para preparar dados para LSTM
+    def prepare_data(self,data, look_back=1):
+        X, Y = [], []
+        for i in range(len(data)-look_back-1):
+            X.append(data[i:(i+look_back), :])
+            Y.append(data[i + look_back, 3])  # 3 corresponde ao índice do preço de fechamento (Close)
+        return np.array(X), np.array(Y)
+
+    # Função para fazer previsões
+    def predict_next_week(self,model, data, days_ahead=7):
+        prediction = []
+        last_sequence = data[-days_ahead:, :]
+        for _ in range(days_ahead):
+            scaled_prediction = model.predict(np.array([last_sequence]))[0][0]
+            unscaled_prediction = self.scaler.inverse_transform([[0, 0, 0, scaled_prediction]])[0][3]
+            prediction.append(unscaled_prediction)
+            last_sequence = np.roll(last_sequence, -1, axis=0)
+            last_sequence[-1, 3] = unscaled_prediction  # Substituir o último preço de fechamento pelo previsto
+        return prediction
+
+    #função que prepara o treino e retorna o modelo
+    def prepar_training_and_model(self,X,Y):
+        # Dividindo os dados em treinamento e teste
+        train_size = int(len(X) * 0.8)
+        test_size = len(X) - train_size
+        X_train, X_test = X[:train_size], X[train_size:]
+        Y_train, Y_test = Y[:train_size], Y[train_size:]
+        # Definindo arquitetura do modelo LSTM
+        model = Sequential()
+        model.add(InputLayer(shape=(self.look_back, 4)))
+        model.add(LSTM(128, return_sequences=True))
+        model.add(Dropout(0.2))  # Adicionando camada de dropout
+        model.add(LSTM(64, return_sequences=False))
+        model.add(Dense(25))
+        model.add(Dense(1))
+        # Compilando o modelo
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        # Ajustando o modelo aos dados de treinamento
+        model.fit(X_train, Y_train, batch_size=2, epochs=self.epochs_database, verbose=2)
+        return model,X_train,Y_train
+    
+    #função que pepara os dados para LSTM
+    def prepar_data_lstm(self,data_features):
+            data_features_scaled = self.scaler.fit_transform(data_features)
+            X, Y = self.prepare_data(data_features_scaled, self.look_back)
+            return X,Y,data_features_scaled
+    #Função que gera dados históricos das ações
+    def get_stock(self,acoes):
+        # Obtendo dados das ações
+                today = date.today()
+                d1 = today.strftime("%Y-%m-%d")
+                end_date = d1
+                d2 = date.today() - timedelta(days=self.dias_base_estudo)
+                d2 = d2.strftime("%Y-%m-%d")
+                start_date = d2
+
+                stock_ticker = acoes
+                data = yf.download(stock_ticker,
+                                    start=start_date,
+                                    end=end_date,
+                                    progress=False)
+                data["Date"] = data.index
+                data = data[["Date", "Open", "High", "Low", "Close",
+                            "Adj Close", "Volume"]]
+                data.reset_index(drop=True, inplace=True)
+                return data
+
+    #analise de precisão do modelo será feito por stock
+    def analisis_results_r2(self,model,X_train,Y_train):
+        # Predict on the training set
+        y_train_pred = model.predict(X_train)
+        # Calculate R^2 score
+        r2 = r2_score(Y_train, y_train_pred)
+        return r2*100
+
+    #gera as previsões dos proximos 7 dias de cada ação salva na lista da classe stockslist   
+    def generate_analisis(self):
+        #captura a lista de ações
+        list_acoes = self.get_list_stocks()
+        #executa previsbildiade com o estudo de modelo em cada ação
+        for acoes in list_acoes:
+            acoes_cleaned = self.sanitize_text(acoes)
+            try:
+                data = self.get_stock(acoes)
+                # Convertendo dataframe para array numpy
+                data_array = data.values
+                # Preparando dados para LSTM
+                data_features = data[['Open', 'High', 'Low', 'Close']].values
+                X,Y,data_features_scaled = self.prepar_data_lstm(data_features)
+                model,X_train,Y_train = self.prepar_training_and_model(X,Y)
+                #analise de precisão do modelo quanto mais proximo de 100% melhor
+                analisis_r2 = self.analisis_results_r2(model,X_train,Y_train)
+                # Fazendo previsões para a próxima semana
+                latest_data = data_features_scaled[-self.look_back:, :]
+                predictions = self.predict_next_week(model, latest_data,days_ahead=7)
+                growth_index = (predictions[-1] / predictions[0] - 1) * 100
+               
+                #guarda as previsões e projeções das ações
+                self.result_dict.update({
+                    acoes_cleaned: {
+                        "predictions": predictions,
+                        "growth_index": growth_index,
+                        "analisis r2":analisis_r2
+                    }
+                })
+
+            #aponta qual erro que ocorreu na ação e salta para a próxima
+            except Exception as e:
+                    print(f"Error occurred for {self.sanitize_text(acoes)}: {e}")
+                    #continue
+        return self.result_dict
+    
+    def save_file(self):
+        # Converter o dicionário em um DataFrame do pandas
+        df = pd.DataFrame.from_dict(self.generate_analisis(), orient='index')
+        # Salvar o DataFrame em um arquivo Excel
+        df.to_excel('./predictions.xlsx',  engine='xlsxwriter')
+
+
+if __name__ == "__main__":
+     analiser = AnalisisStock()
+     file = analiser.save_file()
---
+++ stockslist.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+class Stocklist:
+
+    def __init__(self):
+        self.amostra_acoes = [
+    "ABEV3",
+    "BEEF3",
+    "JBSS3",
+    "ITSA4",
+    "PETR3"
+
+]
+        self.list_completa_acoes = [
+    "DXCO3",
+    "PETZ3",
+    "RRRP3",
+    "RECV3",
+    "PRIO3",
+    "SMTO3",
+    "CSNA3",
+    "CVCB3",
+    "USIM5",
+    "TIMS3",
+    "CYRE3",
+    "RDOR3",
+    "ENEV3",
+    "BRFS3",
+    "TRPL4",
+    "HAPV3",
+    "NTCO3",
+    "CSAN3",
+    "AZUL4",
+    "EMBR3",
+    "ABEV3",
+    "EZTC3",
+    "CMIG4",
+    "MRVE3",
+    "B3SA3",
+    "BEEF3",
+    "ALPA4",
+    "JBSS3",
+    "CPLE6",
+    "ITSA4",
+    "SLCE3",
+    "ITUB4",
+    "UGPA3",
+    "GGBR4",
+    "SBSP3",
+    "BBDC4",
+    "SUZB3",
+    "MGLU3",
+    "HYPE3",
+    "MULT3",
+    "CRFB3",
+    "RAIL3",
+    "CIEL3",
+    "ELET3",
+    "BBDC3",
+    "SOMA3",
+    "ARZZ3",
+    "TOTS3",
+    "ELET6",
+    "BHIA3",
+    "WEGE3",
+    "GOAU4",
+    "ALOS3",
+    "RAIZ4",
+    "RENT3",
+    "LREN3",
+    "RADL3",
+    "LWSA3",
+    "CCRO3",
+    "VBBR3",
+    "EQTL3",
+    "ASAI3",
+    "EGIE3",
+    "BBSE3",
+    "VIVT3",
+    "PCAR3",
+    "CPFE3",
+    "MRFG3",
+    "VALE3",
+    "BRAP4",
+    "BBAS3",
+    "IRBR3",
+    "BRKM5",
+    "YDUQ3",
+    "CMIN3",
+    "COGN3",
+    "VAMO3",
+    "FLRY3",
+    "PETR3",
+    "PETR4"
+]
+
+    def get_list_stocks(self):
+        acoes_SA = [acao + ".SA" for acao in self.list_completa_acoes]
+        return acoes_SA
+
+
+#    
+
